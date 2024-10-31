facae579cfa40cad37fe8eae764c7f62cd75bc2a
Phillip <blauindy@web.de>
2024-10-31 11:14:30 +0100

    inital

---
+++ src/main/java/org/example/DateTimeExample.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class DateTimeExample {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Hello from Dockerized Java application! Current date and time: " + now);
+    }
+}
